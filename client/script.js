9a10fc89da5a7d34aebfd1786e241a3fd84a3809
OUEDRHIRI <ouedrhiri@macs-MacBook-Pro-2.local>
2022-12-27 23:59:15 +0100

    Modifying server

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("https://chatgpt-clone-3x5x.onrender.com", {
+  const response = await fetch({
     method: "POST",
     headers: {
       "Content-Type": "application/json",
